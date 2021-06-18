5dd174ac94f6bfe71053207b1f27f03a1f4d77de
Riccardo Petraglia <riccardo.petraglia@swisscom.com>
2021-06-18 19:47:31 +0200

    feature: automatic first commit

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -9,7 +10,11 @@ def remove_file(filepath):
 
 
 if __name__ == '__main__':
-
+    
+    
+    subprocess.call(('git', 'init'))
+    subprocess.call(('git', 'add', '.'))
+    
     if '{{ cookiecutter.create_author_file }}' != 'y':
         remove_file('AUTHORS.rst')
         remove_file('docs/authors.rst')
@@ -18,11 +23,11 @@ if __name__ == '__main__':
         remove_file('LICENSE')
 
     if '{{ cookiecutter.command_line_interface }}' == 'CLI11':
-        import subprocess
-        subprocess.call(('git', 'init'))
         subprocess.call(('git', 'submodule', 'add', 'git@github.com:CLIUtils/CLI11.git', 'libs/CLI11'))
 
     if '{{ cookiecutter.logging_system }}' == 'y':
-        import subprocess
-        subprocess.call(('git', 'init'))
         subprocess.call(('git', 'submodule', 'add', 'git@github.com:gabime/spdlog.git', 'libs/spdlog'))
+
+
+    subprocess.call(('git', 'commit', '-m', 'Initial commit'))
+
