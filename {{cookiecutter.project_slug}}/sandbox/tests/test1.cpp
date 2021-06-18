9ac62ddc5fac103d464ba7bc43cde811f68d7434
Riccardo Petraglia <riccardo.petraglia@swisscom.com>
2021-06-19 00:05:08 +0200

    fix: sandbox tests need the LOGGER.

--- {{cookiecutter.project_slug}}/sandbox/tests/test1.cpp
+++ {{cookiecutter.project_slug}}/sandbox/tests/test1.cpp
@@ -2,8 +2,11 @@
 #pragma ide diagnostic ignored "cert-err58-cpp"
 #include<gtest/gtest.h>
 
+{% if cookiecutter.logging_system == 'y' -%}#include <Log.h>{% endif %}
 #include<Sandbox.h>
 
+{% if cookiecutter.logging_system == 'y' -%}logging::Log LOGGER = logging::Log();
+{% endif %}
 TEST(Example, ExampleTest){
 Sandbox sandbox = Sandbox();
 ASSERT_STREQ(sandbox.message(), "Hello!");
