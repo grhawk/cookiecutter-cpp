539db9ad99c24046bd5da7485873d0c8e38a03a5
Riccardo Petraglia <riccardo.petraglia@swisscom.com>
2021-06-18 20:05:42 +0200

    fix: logging not there; use `main` branch by default

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import shutil
 import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
@@ -9,15 +10,17 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def remove_folder(folderpath):
+    shutil.rmtree(folderpath)
+
 if __name__ == '__main__':
     
     
-    subprocess.call(('git', 'init'))
+    subprocess.call(('git', 'init', '--initial-branch=main'))
     subprocess.call(('git', 'add', '.'))
     
     if '{{ cookiecutter.create_author_file }}' != 'y':
         remove_file('AUTHORS.rst')
-        remove_file('docs/authors.rst')
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
@@ -27,7 +30,8 @@ if __name__ == '__main__':
 
     if '{{ cookiecutter.logging_system }}' == 'y':
         subprocess.call(('git', 'submodule', 'add', 'git@github.com:gabime/spdlog.git', 'libs/spdlog'))
-
+    else:
+        remove_folder('logging')
 
     subprocess.call(('git', 'commit', '-m', 'Initial commit'))
 
