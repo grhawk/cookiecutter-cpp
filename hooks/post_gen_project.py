1af8a393d73af53a677f2204d3192ea27e6f77c2
Riccardo Petraglia <riccardo.petraglia@swisscom.com>
2021-06-18 17:33:35 +0200

    feature: implemented CLI11

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,14 +14,10 @@ if __name__ == '__main__':
         remove_file('AUTHORS.rst')
         remove_file('docs/authors.rst')
 
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
-
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
 
     if '{{ cookiecutter.command_line_interface }}' == 'CLI11':
         import subprocess
         subprocess.call(('git', 'init'))
-        subprocess.call(('git', 'submodule', 'add', 'git@github.com:CLIUtils/CLI11.git', 'libs/CL11'))
+        subprocess.call(('git', 'submodule', 'add', 'git@github.com:CLIUtils/CLI11.git', 'libs/CLI11'))
--- {{cookiecutter.project_slug}}/sandbox/src/EntryPoint.cpp
+++ {{cookiecutter.project_slug}}/sandbox/src/EntryPoint.cpp
@@ -1,11 +1,20 @@
 
+{% if cookiecutter.command_line_interface == 'CLI11' -%}#include <CLI/CLI.hpp>{% endif %}
 #include <iostream>
 
 #include "Sandbox.h"
 
 int main(int argc, char* argv[])
 {
+    std::string addToMessage = "CLI11 is not activated!";
+    {% if cookiecutter.command_line_interface == 'CLI11' -%}CLI::App cli{"App description"};
+    cli.add_option("Add to message",
+                   addToMessage,
+                   "String to append to message");
+    CLI11_PARSE(cli, argc, argv);{% endif %}
+
     auto* sandbox = new Sandbox();
     std::cout << sandbox->message() << std::endl;
+    std::cout << addToMessage << std::endl;
     delete sandbox;
 }
